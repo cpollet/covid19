d1d730676a805b78bd50ca162f63b4b080355e6f
Christophe Pollet <cpollet@users.noreply.github.com>
2020-09-06 11:33:09 +0200

    Implement gcloud HttpFunction

--- stats-loader/src/main/java/net/cpollet/covid19/statsloader/App.java
+++ stats-loader/src/main/java/net/cpollet/covid19/statsloader/App.java
@@ -1,5 +1,8 @@
 package net.cpollet.covid19.statsloader;
 
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
 import net.cpollet.covid19.statsloader.data.Source;
 import net.cpollet.covid19.statsloader.data.apfeuti.ApDataSupplier;
 import net.cpollet.covid19.statsloader.data.apfeuti.ApPointSource;
@@ -11,7 +14,7 @@ import org.influxdb.InfluxDB;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
-public class App {
+public class App implements HttpFunction {
     public static void main(String[] args) {
         InfluxDB influxDB = InfluxDBFactory.covid19();
 
@@ -22,5 +25,10 @@ public class App {
                 .parallel()
                 .flatMap(Source::stream)
                 .forEach(influxDB::write);
+    }
+
+    @Override
+    public void service(HttpRequest httpRequest, HttpResponse httpResponse) {
+        App.main(new String[]{});
     }
 }
