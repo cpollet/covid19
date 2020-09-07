a4424d696af5cc16aea8c704a0ecf912a7bdca11
Christophe Pollet <cpollet@users.noreply.github.com>
2020-09-07 19:50:14 +0200

    Implement gcloud HttpFunction

---
+++ stats-loader/README.md
@@ -0,0 +1,5 @@
+# stats-loader
+```
+export INFLUXDB_HOST=34.65.136.121 && mvn clean install function:run
+curl localhost:8080  
+```
--- stats-loader/src/main/java/net/cpollet/covid19/statsloader/App.java
+++ stats-loader/src/main/java/net/cpollet/covid19/statsloader/App.java
@@ -1,5 +1,8 @@
 package net.cpollet.covid19.statsloader;
 
+import com.google.cloud.functions.HttpFunction;
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
 import net.cpollet.covid19.statsloader.data.LastUpdateSource;
 import net.cpollet.covid19.statsloader.data.Source;
 import net.cpollet.covid19.statsloader.data.apfeuti.ApDataSupplier;
@@ -7,21 +10,46 @@ import net.cpollet.covid19.statsloader.data.apfeuti.ApPointSource;
 import net.cpollet.covid19.statsloader.data.foph.FophDataSupplier;
 import net.cpollet.covid19.statsloader.data.foph.FophPointSource;
 import net.cpollet.covid19.statsloader.db.InfluxDBFactory;
-import org.influxdb.InfluxDB;
+import org.influxdb.dto.BatchPoints;
+import org.influxdb.dto.Point;
 
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class App {
-    public static void main(String[] args) {
-        InfluxDB influxDB = InfluxDBFactory.covid19();
+public class App implements HttpFunction {
+    private static Logger LOGGER = Logger.getLogger(App.class.getName());
 
-        Stream.of(
+    public static void main(String[] args) {
+        new App().doWork();
+    }
+
+    private void doWork() {
+        long startTime = System.currentTimeMillis();
+
+        LOGGER.info("Computing points");
+        List<Point> points = Stream.of(
                 new ApPointSource(new ApDataSupplier()),
-                new FophPointSource(new FophDataSupplier()),
-                new LastUpdateSource()
+                new FophPointSource(new FophDataSupplier())
         )
                 .parallel()
                 .flatMap(Source::stream)
-                .forEach(influxDB::write);
+                .collect(Collectors.toList());
+
+        LOGGER.info("Sending points to influxdb");
+        InfluxDBFactory.covid19().write(
+                BatchPoints.builder()
+                        .points(points)
+                        .points(new LastUpdateSource().stream().collect(Collectors.toList())).build()
+        );
+
+        long duration = System.currentTimeMillis() - startTime;
+        LOGGER.info(String.format("Finised in %d ms", duration));
+    }
+
+    @Override
+    public void service(HttpRequest httpRequest, HttpResponse httpResponse) {
+        doWork();
     }
 }
